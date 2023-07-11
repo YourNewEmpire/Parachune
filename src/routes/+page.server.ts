81f2ca74d12bde05a0c9aee0a30ddb67f2ab1ca9
YourNewEmpire <archiesmyth26@googlemail.com>
2023-07-11 14:10:07 +0100

    chore: remove console.logs & unused vars in index +page.server

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,14 +2,12 @@ import { fail, redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({
-  url,
   locals: { supabase, getSession, getProfile },
 }) => {
   const session = await getSession();
   const profile = await getProfile();
   let message;
-  console.log(session);
-  console.log(`hello ${session?.user} are you there?`);
+
   if (!session) {
     message = `Welcome newcomer. This project is a spotify clone. you can upload songs for free, (donations coming soon). 
       Click the button below to begin your music journey`;
@@ -20,7 +18,6 @@ export const load: PageServerLoad = async ({
   Navigate to Your Profile to start uploading music.`;
   }
   return {
-    url: url.origin,
     message,
   };
 };
