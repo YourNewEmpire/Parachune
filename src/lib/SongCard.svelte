2665a2e88e524ca83159063bf7a173c09f4ef0ba
YourNewEmpire <archiesmyth26@googlemail.com>
2023-07-18 12:45:38 +0100

    chore/feat: move markup from discover into SongCard component

---
+++ src/lib/SongCard.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import AvatarIcon from "$lib/AvatarIcon.svelte";
+  import Playbutton from "$lib/Playbutton.svelte";
+  import Queuebutton from "$lib/Queuebutton.svelte";
+  import Savebutton from "$lib/Savebutton.svelte";
+  import type { SupabaseClient } from "@supabase/supabase-js";
+  type SongRow = {
+    artist: string | null;
+    created_at: string | null;
+    id: number;
+    name: string | null;
+    song_url: string | null;
+    profiles: {
+      avatar_url: string;
+    };
+  };
+  export let supabase: SupabaseClient;
+  export let song: SongRow;
+</script>
+
+<div class="card">
+  <div class="row-container">
+    <div class="row-container">
+      <Playbutton songUrl={song.song_url ?? ""} />
+      <Queuebutton songUrl={song.song_url ?? ""} />
+      <Savebutton songUrl={song.song_url ?? ""} songId={song.id ?? ""} />
+    </div>
+    <div class="song-info">
+      <p>
+        {song.name}
+      </p>
+      <div class="row-container">
+        <AvatarIcon size={5} {supabase} url={song.profiles.avatar_url} />
+        <p>
+          {song.artist}
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .song-info {
+    display: flex;
+    flex-direction: column;
+    row-gap: 6px;
+  }
+</style>
--- src/routes/discover/+page.svelte
+++ src/routes/discover/+page.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
   import type { PageData } from "./$types";
-  import Playbutton from "$lib/Playbutton.svelte";
-  import Savebutton from "$lib/Savebutton.svelte";
-  import Queuebutton from "$lib/Queuebutton.svelte";
-  import AvatarIcon from "$lib/AvatarIcon.svelte";
+  import SongCard from "$lib/SongCard.svelte";
   export let data: PageData;
   let { dbData, supabase } = data;
 </script>
@@ -16,34 +13,7 @@
   <h1>Discover</h1>
   <div class="card-col-container">
     {#each dbData as song}
-      <div class="card">
-        <div class="row-container">
-          <div class="row-container">
-            <Playbutton songUrl={song.song_url ?? ""} />
-            <Queuebutton songUrl={song.song_url ?? ""} />
-            <Savebutton songUrl={song.song_url ?? ""} songId={song.id ?? ""} />
-          </div>
-          <div class="song-info">
-            <p>
-              {song.name}
-            </p>
-            <div class="row-container">
-              <AvatarIcon size={5} {supabase} url={song.profiles.avatar_url} />
-              <p>
-                {song.artist}
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <SongCard {supabase} {song} />
     {/each}
   </div>
 </div>
-
-<style>
-  .song-info {
-    display: flex;
-    flex-direction: column;
-    row-gap: 6px;
-  }
-</style>
