07e65ddd51e4f2e550481e18cc1527b3e4b19a7f
YourNewEmpire <archiesmyth26@googlemail.com>
2023-07-21 15:02:24 +0100

    feat: times added to player & using css tooltip

--- src/lib/Player.svelte
+++ src/lib/Player.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
   import { fly } from "svelte/transition";
   import { songsQueued, songPlaying, songsPlayed, addToast } from "$lib/stores";
-  import { Icon, Play, Pause, Backward, Forward } from "svelte-hero-icons";
+  import {
+    Icon,
+    Play,
+    Pause,
+    Backward,
+    Forward,
+    XCircle,
+  } from "svelte-hero-icons";
   import type { SupabaseClient } from "@supabase/supabase-js";
 
   export let sClient: SupabaseClient;
@@ -34,6 +41,7 @@
         audioBind.load();
         audioBind.play();
         $songPlaying = true;
+        paused = false;
       }
     } catch (e) {
       // todo - push error toaster
@@ -54,19 +62,22 @@
   // click handlers
   function endPlayer() {
     audioBind.pause();
+    audioBind.currentTime = 0;
     isOpen = false;
     $songPlaying = false;
+    paused = true;
     $songsQueued = [];
     $songsPlayed = [];
   }
 
   function handlePause() {
-    if (paused) {
+    if (audioBind.paused) {
       audioBind.play();
+      paused = false;
     } else {
       audioBind.pause();
+      paused = true;
     }
-    paused = !paused;
   }
   // todo - USING DOLLAR SYMBOL TO READ AND UPDATE STATE
   function handleForward() {
@@ -109,7 +120,7 @@
     return `${minutes}:${seconds}`;
   }
 
-  $: if (scrubBind && isOpen)
+  $: if ($songPlaying == true)
     scrubBind.style.backgroundSize =
       ((time - 0) * 100) / (duration - 0) + "% 100%";
 
@@ -124,7 +135,8 @@
     class="player-wrapper"
   >
     <div class="controls">
-      <div>
+      <div class="scrub">
+        <p>{formatTime(time)}</p>
         <input
           type="range"
           bind:this={scrubBind}
@@ -132,33 +144,53 @@
           min="0"
           max={duration}
         />
+        <p>{formatTime(duration)}</p>
       </div>
       <div class="buttons">
-        <button class="styled-button" on:click={handleBack}>
+        <button
+          class="styled-button"
+          data-tooltip="Backward"
+          on:click={handleBack}
+        >
           <Icon style="width: 2rem;" src={Backward} />
         </button>
         {#if paused}
-          <button class="styled-button" on:click={handlePause}>
+          <button
+            class="styled-button"
+            data-tooltip="Play"
+            on:click={handlePause}
+          >
             <Icon style="width: 2rem;" src={Play} />
           </button>
         {:else}
-          <button class="styled-button" on:click={handlePause}>
+          <button
+            class="styled-button"
+            data-tooltip="Pause"
+            on:click={handlePause}
+          >
             <Icon style="width: 2rem;" src={Pause} />
           </button>
         {/if}
-        <button class="styled-button" on:click={handleForward}>
+        <button
+          class="styled-button"
+          data-tooltip="Forward"
+          on:click={handleForward}
+        >
           <Icon style="width: 2rem;" src={Forward} />
         </button>
+        <button
+          class="styled-button"
+          style="position: absolute; right: 0;"
+          on:click={endPlayer}
+        >
+          <Icon src={XCircle} style="width: 2rem;" />
+          Close Player
+        </button>
       </div>
-      <button
-        style="position: absolute; bottom: 30px; right: 0;"
-        on:click={endPlayer}>X Stop listening</button
-      >
     </div>
 
     <audio autoplay bind:this={audioBind} bind:currentTime={time} bind:duration>
       <source type="audio/mp3" />
-
       Your browser does not support the audio element.
     </audio>
   </div>
@@ -169,6 +201,7 @@
     display: block;
     position: fixed;
     margin-left: 250px;
+    padding: 0 5px;
     bottom: 0;
     left: 0;
     width: calc(100% - 250px);
@@ -176,10 +209,18 @@
     background-color: #9898ac;
     box-shadow: 0px 0px 6px #856bdc;
   }
+
   .controls {
     padding: 1rem 1rem;
     display: block;
     position: relative;
+  }
+
+  .scrub {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
   }
 
   .buttons {
@@ -187,5 +228,6 @@
     justify-content: center;
     align-items: center;
     flex-direction: row;
+    column-gap: 4px;
   }
 </style>
