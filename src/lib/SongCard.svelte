50499e7ec191398bfb15b6633aa4de82ccae39cc
YourNewEmpire <archiesmyth26@googlemail.com>
2023-07-31 17:54:48 +0100

    feat: Songcard styles to be corrected for card grid layout

--- src/lib/SongCard.svelte
+++ src/lib/SongCard.svelte
@@ -3,7 +3,6 @@
   import Playbutton from "$lib/Playbutton.svelte";
   import Queuebutton from "$lib/Queuebutton.svelte";
   import Savebutton from "$lib/Savebutton.svelte";
-  import type { SupabaseClient } from "@supabase/supabase-js";
   type SongRow = {
     artist: string | null;
     created_at: string | null;
@@ -14,34 +13,95 @@
       avatar_url: string;
     };
   };
-  export let song: SongRow;
+  export let song: SongRow | any;
 </script>
 
-<div class="card">
-  <div class="row-container">
-    <div class="row-container">
+<div class="song-card">
+  <div class="song-card-container">
+    <div class="buttons">
       <Playbutton songUrl={song.song_url ?? ""} />
       <Queuebutton songUrl={song.song_url ?? ""} />
       <Savebutton songUrl={song.song_url ?? ""} songId={song.id ?? ""} />
     </div>
     <div class="song-info">
-      <p>
+      <a
+        style="white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      max-width: 25ch;
+     "
+        class="styled-link"
+        href="/songs/{song.name}"
+      >
         {song.name}
-      </p>
-      <div class="row-container">
-        <AvatarIcon size={5} url={song.profiles.avatar_url} />
-        <p>
+      </a>
+      <a class="profile-link" href="/profiles/{song.artist}">
+        <AvatarIcon size={2} url={song.profiles.avatar_url} />
+
+        <p style="color: #333;">
           {song.artist}
         </p>
-      </div>
+      </a>
     </div>
   </div>
 </div>
 
 <style>
+  .song-card {
+    background-color: #9898ac;
+    box-shadow: 0px 0px 8px #856bdc;
+    border-radius: 0.5rem;
+    padding: 1rem 1rem;
+    color: #1a1a26;
+  }
+  .song-card-container {
+    display: flex;
+    flex-direction: column;
+    row-gap: 2rem;
+    align-items: center;
+  }
   .song-info {
     display: flex;
     flex-direction: column;
     row-gap: 6px;
+    width: 100%;
+    max-width: 350px;
+  }
+  .profile-link {
+    display: flex;
+    width: fit-content;
+    flex-direction: row;
+    align-items: center;
+    column-gap: 12px;
+    border: none;
+    background: none;
+    color: inherit;
+    text-decoration: none;
+    cursor: pointer;
+    border-radius: 0.75rem;
+    transition: all 0.3s ease;
+  }
+  .profile-link:hover {
+    color: #856bdc;
+    text-decoration: underline;
+  }
+  .buttons {
+    display: flex;
+    flex-direction: row;
+    column-gap: 0.25rem;
+  }
+  @media only screen and (min-width: 768px) {
+    .buttons {
+      column-gap: 0.5rem;
+    }
+    .song-card-container {
+      flex-direction: row;
+      column-gap: 2rem;
+    }
+  }
+  @media only screen and (min-width: 1024px) {
+    .buttons {
+      column-gap: 1rem;
+    }
   }
 </style>
