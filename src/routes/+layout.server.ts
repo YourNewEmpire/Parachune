9126c75c6c31ad787466721bc7e1c7511f2e835f
YourNewEmpire <archiesmyth26@googlemail.com>
2023-07-31 17:55:01 +0100

    feat: pass profile from +layout.server.ts

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,10 @@
 import type { LayoutServerLoad } from "./$types";
 
-export const load: LayoutServerLoad = async ({ locals: { getSession } }) => {
+export const load: LayoutServerLoad = async ({
+  locals: { getSession, getProfile },
+}) => {
   return {
     session: getSession(),
+    profile: getProfile(),
   };
 };
