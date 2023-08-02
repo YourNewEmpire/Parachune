aa07e7083368b9237328b99dfd0b238345add2ea
YourNewEmpire <archiesmyth26@googlemail.com>
2023-08-02 18:55:24 +0100

    feat: finish songs index layout

--- src/routes/songs/+page.svelte
+++ src/routes/songs/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import SongCard from "$lib/SongCard.svelte";
+  import SongCard from "$lib/songcard.svelte";
   import type { PageData } from "./$types";
   import { page } from "$app/stores";
 
@@ -13,23 +13,39 @@
 
 <div>
   <h1>All Songs</h1>
-  <div class="card-col-container">
+  <section class="card-container">
     {#each songList ?? [] as song}
       <SongCard {song} />
     {/each}
-  </div>
-  <div style="display: flex; flex-direction: row; column-gap: 2px;">
-    {#each Array(totalPages) as _, idx}
-      <a
-        data-sveltekit-reload
-        href="/songs?limit={pageSize * idx + pageSize}&skip={pageSize * idx}"
-        class="styled-link"
-        style=" {currentPage === idx
-          ? 'text-decoration: underline; color: #856bdc; font-size: 2em'
-          : 'font-size: 2em'} "
-      >
-        {idx + 1}
-      </a>
-    {/each}
-  </div>
+  </section>
+  {#if totalPages >= 2}
+    <nav style="display: flex; flex-direction: row; column-gap: 2px;">
+      {#each Array(totalPages) as _, idx}
+        <a
+          data-sveltekit-reload
+          href="/songs?limit={pageSize * idx + pageSize}&skip={pageSize * idx}"
+          class="styled-link"
+          style=" {currentPage === idx
+            ? 'text-decoration: underline; color: #856bdc; font-size: 2em'
+            : 'font-size: 2em'} "
+        >
+          {idx + 1}
+        </a>
+      {/each}
+    </nav>
+  {/if}
 </div>
+
+<style>
+  .card-container {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, 250px);
+    grid-gap: 12px;
+    justify-content: center;
+  }
+  @media only screen and (min-width: 1024px) {
+    .card-container {
+      justify-content: start;
+    }
+  }
+</style>
