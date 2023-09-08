6a081d912d1076ea379f426c70504481558c3003
YourNewEmpire <archiesmyth26@googlemail.com>
2023-09-08 16:38:08 +0100

    feat: likes/dislikes added to song slug

--- src/routes/songs/[slug]/+page.server.ts
+++ src/routes/songs/[slug]/+page.server.ts
@@ -1,4 +1,4 @@
-import { fail, redirect } from "@sveltejs/kit";
+import { error, redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 type SongRow = {
@@ -11,19 +11,64 @@ type SongRow = {
   lyrics: string | null;
   profiles: {
     avatar_url: string;
+    username: string;
   };
+  likes: number;
+  isLiked: boolean | "unset";
 };
 export const load: PageServerLoad = async ({
-  locals: { supabase },
+  locals: { supabase, getSession },
   params,
 }) => {
-  const { data: songData, error: songError } = await supabase
+  const session = await getSession();
+  let isLiked: boolean | "unset" = "unset";
+
+  const { data: songData, error: songDataErr } = await supabase
     .from("songs")
     .select(
       `artist, name, description, lyrics, song_url, created_at, id, profiles ( avatar_url, username)`
     )
     .eq("name", params.slug)
     .single();
-  let song: SongRow | any = songData;
+
+  if (songDataErr) {
+    throw error(404, {
+      message: "Song not found",
+    });
+  }
+
+  const {
+    data: songLikes,
+    error: songLikesErr,
+    count,
+  } = await supabase
+    .from("song_likes")
+    .select(`liked`, { count: "exact", head: true })
+    .eq("song_id", songData?.id)
+    .eq("liked", true);
+
+  if (songLikesErr) {
+    throw error(500, {
+      message: "Server error when getting song data",
+    });
+  }
+  // this will block unauthed !!!!!!!!!
+  if (!session) {
+    let song: SongRow | any = { ...songData, likes: count ?? 0, liked: null };
+
+    return { song };
+  }
+  const { data: userLiked, error: userLikedErr } = await supabase
+    .from("song_likes")
+    .select(`user_id, liked`)
+    .eq("user_id", session?.user.id)
+    .single();
+
+  if (userLiked) {
+    // console.log(`server side user liked result ${userLiked.liked}`);
+    isLiked = userLiked.liked;
+  }
+  let song: SongRow | any = { ...songData, likes: count ?? 0, liked: isLiked };
+
   return { song };
 };
--- src/routes/songs/[slug]/+page.svelte
+++ src/routes/songs/[slug]/+page.svelte
@@ -5,19 +5,66 @@
   import Playbutton from "$lib/Playbutton.svelte";
   import Queuebutton from "$lib/Queuebutton.svelte";
   import Savebutton from "$lib/Savebutton.svelte";
+  import { addToast } from "$lib/stores";
 
   export let data: PageData;
   let { song, supabase, session } = data;
 
   async function setLike(payload: boolean) {
-    const { data, error: likeError } = await supabase
+    // TODO - fetch if the user has already liked this. using the isLiked state from song data
+    if (!session) {
+      addToast({
+        dismissable: true,
+        message:
+          "You are not logged in, try refreshing and navigating to login",
+        timeout: 5000,
+        type: "failure",
+      });
+      return;
+    }
+
+    if (song.liked === payload) {
+      const { error: delErr } = await supabase
+        .from("song_likes")
+        .delete()
+        .eq("user_id", session?.user.id)
+        .eq("song_id", song.id);
+      //remove the row
+      if (delErr) {
+        addToast({
+          dismissable: true,
+          message: "Error removing like.",
+          timeout: 5000,
+          type: "failure",
+        });
+        return;
+      }
+      song.liked = null;
+      addToast({
+        dismissable: true,
+        message: "Removed like",
+        timeout: 5000,
+        type: "info",
+      });
+      return;
+    }
+    const { data: likedUpdated, error: likeError } = await supabase
       .from("song_likes")
       .upsert(
         { song_id: song.id, user_id: session?.user.id, liked: payload },
         { onConflict: "song_id, user_id" }
       )
       .select();
-    console.log(likeError);
+    if (likeError) {
+      addToast({
+        dismissable: true,
+        message: "error updating like",
+        timeout: 5000,
+        type: "failure",
+      });
+      return;
+    }
+    song.liked = likedUpdated[0].liked;
   }
 </script>
 
@@ -51,21 +98,53 @@
     </article>
     <article class="card">
       <a href="/profiles/{song.profiles.username}">
-        <AvatarIcon size={5} url={song.profiles.avatar_url} />
+        <AvatarIcon
+          altText={song.profiles.username}
+          size={5}
+          url={song.profiles.avatar_url}
+        />
       </a>
       <p>"{song.description}"</p>
     </article>
   </section>
-  <h1>Reviews & Likes</h1>
-  <section class="grid-1">
-    <article class="card">
-      <h1>reviews</h1>
-    </article>
-    <article class="card">
-      likes: dislikes:
-      <button on:click={() => setLike(true)}> like</button>
-      <button on:click={() => setLike(false)}> dislike</button>
-    </article>
+  <section>
+    <h1>Reviews & Likes</h1>
+    <div class="grid-1">
+      <article class="card">
+        <h1>reviews</h1>
+      </article>
+      <article class="card likes-container">
+        <div class="likes">
+          Likes: {song.likes}
+        </div>
+        {#if song.liked === true}
+          <button
+            class="like-button"
+            data-tooltip="Click to remove like"
+            on:click={() => setLike(true)}>Liked!</button
+          >
+          <button class="dislike-button" on:click={() => setLike(false)}
+            >Dislike</button
+          >
+        {:else if song.liked === false}
+          <button class="like-button" on:click={() => setLike(true)}
+            >Like</button
+          >
+          <button
+            data-tooltip="Click to remove dislike"
+            class="dislike-button"
+            on:click={() => setLike(false)}>Disliked!</button
+          >
+        {:else}
+          <button class="like-button" on:click={() => setLike(true)}
+            >Like</button
+          >
+          <button class="dislike-button" on:click={() => setLike(false)}
+            >Dislike</button
+          >
+        {/if}
+      </article>
+    </div>
   </section>
 </div>
 
@@ -82,5 +161,43 @@
       grid-template-rows: 1fr;
       grid-template-columns: 3fr 1fr;
     }
+  }
+  .likes-container {
+    display: flex;
+    flex-direction: column;
+    row-gap: 0;
+    padding: 0;
+  }
+  .likes {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 18px;
+  }
+  .like-button {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 18px;
+    color: blue;
+  }
+  .dislike-button {
+    border-radius: 0 0 0.5rem 0.5rem;
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 18px;
+    color: red;
+  }
+  .like-button:hover {
+    color: white;
+    background-color: rgba(0, 100, 222, 0.6);
+  }
+  .dislike-button:hover {
+    color: white;
+    background-color: rgba(199, 0, 0, 0.6);
   }
 </style>
