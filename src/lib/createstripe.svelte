b054d88b54cddbc09ce35b3d2fcac2933dadcf69
YourNewEmpire <archiesmyth26@googlemail.com>
2023-11-01 15:38:25 +0000

    fix: pass the account link url, not entire object

--- src/lib/createstripe.svelte
+++ src/lib/createstripe.svelte
@@ -20,18 +20,21 @@
         message: "No response from server",
         timeout: 10000,
       });
+      return;
     }
 
     const stripeLink = await response.json();
-    if (!stripeLink) {
+    if (!stripeLink.url) {
       addToast({
         type: "failure",
         dismissable: false,
         message: "There was a problem creating stripe account for Parachune",
         timeout: 10000,
       });
+      return;
     }
-   window.location.href = stripeLink.url
+
+    window.location.href = stripeLink.url;
   }
 </script>
 
