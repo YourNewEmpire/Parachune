c4bbceec65bf8c4038910d6387115ed0094b330c
YourNewEmpire <archiesmyth26@googlemail.com>
2023-12-14 18:04:12 +0000

    feat: upload page working, styling to be polished

--- src/routes/account/upload/+page.server.ts
+++ src/routes/account/upload/+page.server.ts
@@ -2,18 +2,26 @@ import { fail, redirect } from "@sveltejs/kit";
 import { v4 as uuidv4 } from "uuid";
 import type { Actions, PageServerLoad } from "./$types";
 export const load: PageServerLoad = async ({
-  locals: { getSession, getProfile },
+  locals: { getSession, supabase },
+  parent,
 }) => {
   const session = await getSession();
-  const profile = await getProfile();
   if (!session) {
     throw redirect(303, "/login");
   }
 
-  return { session, profile };
+  const { data: albumData, error: albumError } = await supabase
+    .from("albums")
+    .select("*")
+    .eq("artist_id", session.user.id);
+
+  //todo - FETCH ALBUM DATA FOR THE USER FOR THE ALBUM FORM SELECT
+
+  const { profile } = await parent();
+  return { session, profile, albumData };
 };
 
-export const actions = {
+export const actions: Actions = {
   uploadSong: async ({ request, locals: { supabase, getSession } }) => {
     const session = await getSession();
     if (!session) {
@@ -24,31 +32,30 @@ export const actions = {
     const formData = await request.formData();
     const songFile = formData.get("song") as File;
     const songName = formData.get("songName") as string;
+    const songAlbum = formData.get("songAlbum") as string;
 
     // Validate form
     if (songName.length < 1) {
-      return fail(400, {
-        message: "Song name too short, at least 1 char please",
-      });
+      return fail(400);
     }
     if (songFile.size < 1) {
-      return fail(400, {
-        message: "Please atttach a file",
-      });
+      return fail(400);
     }
-
+    if (songAlbum.length < 1) {
+      return fail(400);
+    }
     // Prepare song file
     const fileExt = songFile.name.split(".").pop();
     let uId = uuidv4();
-    let url = `${songName.split(" ").join("") + uId}.${fileExt}`;
+    let url = `${uId}.${fileExt}`;
 
-    // //? Upload and insert to supabase
+    //? Upload and insert to supabase
     const { data, error } = await supabase.storage
       .from("songs")
       .upload(url, songFile);
     if (error) {
       // console.log(error);
-      return fail(400, { message: "Failed to set DB entry, try again." });
+      return fail(400);
     }
     const { data: dbData, error: dbError } = await supabase
       .from("songs")
@@ -57,15 +64,70 @@ export const actions = {
           name: songName,
           artist_id: session.user.id,
           song_url: url,
+          album_id: songAlbum,
         },
       ]);
     if (dbError) {
       // console.log(dbError);
-      return fail(400, { message: "Failed to set DB entry, try again." });
+      return fail(400);
     }
     //? No errors were caught, so return good
     return {
-      message: "Song complete, check Your Music",
+      message: "Song complete, check Music",
     };
   },
-} satisfies Actions;
+
+  createAlbum: async ({ request, locals: { supabase, getSession } }) => {
+    const session = await getSession();
+    if (!session) {
+      return fail(400, {
+        message: "Please login",
+      });
+    }
+
+    const formData = await request.formData();
+    const albumName = formData.get("albumName") as string;
+    const albumImage = formData.get("albumImage") as File;
+    const albumType = formData.get("albumType") as string;
+    const albumGenre = formData.get("albumGenre") as string;
+    if (albumName.length < 1) {
+      return fail(400);
+    }
+    if (albumImage.size < 1) {
+      return fail(400);
+    }
+    // Prepare song file
+    const fileExt = albumImage.name.split(".").pop();
+    let uId = uuidv4();
+    let url = `${uId}.${fileExt}`;
+    const { data, error } = await supabase.storage
+      .from("album-images")
+      .upload(url, albumImage);
+    if (error) {
+      // console.log(error);
+      return fail(400);
+    }
+    const { data: dbData, error: dbError } = await supabase
+      .from("albums")
+      .insert([
+        {
+          title: albumName,
+          artist_id: session.user.id,
+          image_url: url,
+          is_single: albumType === "single" ? true : false,
+          genre: albumGenre,
+        },
+      ])
+      .select();
+    if (dbError) {
+      // console.log(dbError);
+      return fail(400);
+    }
+    console.log(dbData);
+    //? No errors were caught, so return good
+    return {
+      message: "Album complete, go upload to it.",
+      newAlbum: dbData[0],
+    };
+  },
+};
--- src/routes/account/upload/+page.svelte
+++ src/routes/account/upload/+page.svelte
@@ -1,70 +1,136 @@
 <script lang="ts">
   import { enhance } from "$app/forms";
-  import type { PageData } from "./$types";
+  import { addToast } from "$lib/stores";
+  import type { PageData, SubmitFunction } from "./$types";
   export let data: PageData;
 
-  let { session, profile } = data;
+  let { session, profile, albumData } = data;
+  // not sure if form binding is needed
   let uploadForm: any;
   let loading = false;
-  let formMessage = "";
-  function handleSubmit() {
-    loading = true;
+  let uploadFormMsg: string;
+  let albumFormMsg: string;
 
-    return async ({ result }: { result: any }) => {
-      uploadForm.reset();
-      formMessage = result.data.message;
+  const handleSubmit: SubmitFunction = () => {
+    loading = true;
+    return async ({ result, update }) => {
+      if (result.type === "success") {
+        //@ts-ignore
+        if (result.data.newAlbum) {
+          //@ts-ignore
+          albumData = [...albumData, result.data.newAlbum];
+        }
+        addToast({
+          dismissable: true,
+          message: result.data?.message ?? "Action complete",
+          timeout: 5000,
+          type: "success",
+        });
+      } else {
+        addToast({
+          dismissable: true,
+          message:
+            "Failed upload, check you have filled the form. Contact support if there is a clear issue.",
+          timeout: 5000,
+          type: "failure",
+        });
+      }
+      update();
       loading = false;
     };
-  }
-  function handleInput(e: any) {
+  };
+
+  function handleUploadInput(e: any) {
     if (e.target.value < 1) {
-      formMessage = "Song name must be at least 1 char";
+      uploadFormMsg = "Song title must have at least 1 char";
     } else {
-      formMessage = "";
+      uploadFormMsg = "";
     }
   }
-  // onMount(() => {
-  //   supabase
-  //     .channel("any")
-  //     .on(
-  //       "postgres_changes",
-  //       {
-  //         event: "*",
-  //         schema: "public",
-  //         table: "songs",
-  //         filter: `artist=eq.${profile.username}`,
-  //       },
-  //       (payload) => {
-  //         console.log("Change received!", payload);
-  //
-  //         //@ts-ignore
-  //         songs = [...songs, payload.new];
-  //       }
-  //     )
-  //     .subscribe();
-
-  //   return async () => supabase.removeAllChannels();
-  // });
+  function handleAlbumInput(e: any) {
+    if (e.target.value < 1) {
+      albumFormMsg = "Album title must have at least 1 char";
+    } else {
+      albumFormMsg = "";
+    }
+  }
 </script>
 
-<h1>Upload a song</h1>
-<div style="display: flex; justify-content: center; ">
-  <section class="card">
+<section>
+  <h1>Upload a song or create an album</h1>
+  <div style="display: flex; justify-content: center; gap: 2em;">
     <form
-      class="input-group"
+      class="input-group card"
       method="post"
       action="?/uploadSong"
       use:enhance={handleSubmit}
-      bind:this={uploadForm}
     >
       <div class="input-item">
         <label for="songName">Song Name</label>
-        <input on:input={handleInput} type="text" name="songName" id="" />
+        <input on:input={handleUploadInput} type="text" name="songName" id="" />
       </div>
+
       <div class="input-item">
         <label for="song">Song File</label>
         <input type="file" name="song" />
       </div>
+
+      <div class="input-item">
+        <label for="songAlbum">Song Album</label>
+        <select
+          name="songAlbum"
+          id="songAlbum"
+          disabled={albumData?.length ?? [].length > 1 ? false : true}
+        >
+          {#each albumData ?? [] as a}
+            <option value={a.id}>{a.title}</option>
+          {/each}
+          <!-- todo - MAP OVER ALBUMS FROM DATA AND RENDER -->
+        </select>
+      </div>
+
+      <span style="color: red;">{uploadFormMsg ?? ""}</span>
+
+      <button
+        class="styled-button"
+        style="font-family: Sono, sans-serif;"
+        disabled={loading}
+        type="submit"
+      >
+        Upload
+      </button>
+    </form>
+
+    <form
+      class="input-group card"
+      method="post"
+      action="?/createAlbum"
+      use:enhance={handleSubmit}
+    >
+      <div class="input-item">
+        <label for="albumName">Album Name</label>
+        <input on:input={handleAlbumInput} type="text" name="albumName" />
+      </div>
+      <div class="input-item">
+        <label for="albumImage">Album Image</label>
+        <input type="file" name="albumImage" />
+      </div>
+      <div class="input-item">
+        <label for="albumGenre">Album Genre (Optional)</label>
+        <input type="text" name="albumGenre" />
+      </div>
+      <div class="upload-radio">
+        <article>
+          <input type="radio" name="albumType" id="typeAlbum" value="album" />
+          <label for="typeAlbum">Album</label>
+        </article>
+        <article>
+          <input type="radio" name="albumType" id="typeSingle" value="single" />
+          <label for="typeSingle">Single</label>
+        </article>
+      </div>
+
+      <span style="color: red;">{albumFormMsg ?? ""}</span>
       <button
         class="styled-button"
         style="font-family: Sono, sans-serif;"
@@ -72,5 +138,44 @@
         type="submit">Upload</button
       >
     </form>
-  </section>
-</div>
+  </div>
+</section>
+
+<style>
+  .upload-radio {
+    display: grid;
+    grid-template-columns: 50% 50%;
+  }
+  .upload-radio article {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .upload-radio label {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+    padding: 0.5em 0.5em;
+    font-size: 2em;
+    cursor: pointer;
+    transition: all 0.1s ease;
+    border-radius: 0.5em;
+    transform: scale(1);
+  }
+  .upload-radio input[type="radio"] {
+    visibility: hidden;
+    display: none;
+  }
+  .upload-radio label:hover {
+    background-color: var(--primary-color);
+    box-shadow: 0 0 8px var(--primary-color);
+  }
+  .upload-radio input[type="radio"]:checked + label {
+    transition: all 0.3s ease;
+    border-radius: 2em;
+    transform: scale(0.8);
+    background-color: var(--primary-color);
+    box-shadow: 0 0 8px var(--primary-color);
+  }
+</style>
