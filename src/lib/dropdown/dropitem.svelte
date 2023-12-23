bf99784bc75df77a780edc051e424ee9a935bd3e
YourNewEmpire <archiesmyth26@googlemail.com>
2023-12-23 14:38:27 +0000

    chore: remove comment

--- src/lib/dropdown/dropitem.svelte
+++ src/lib/dropdown/dropitem.svelte
@@ -2,11 +2,11 @@
 
 <style>
   li {
+    /* transition: all 0.4s cubic-bezier(0.86, 0.01, 0.15, 0.99); */
     position: relative;
     display: block;
     transition-duration: 0.2s;
   }
-
   li:hover,
   li:focus {
     background-color: rgba(0, 0, 0, 0.5);
