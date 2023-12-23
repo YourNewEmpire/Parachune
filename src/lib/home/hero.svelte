e610a813583a242299034e501042cc4d62a79a49
YourNewEmpire <archiesmyth26@googlemail.com>
2023-12-23 14:38:47 +0000

    feat: add hero component & items for home page

---
+++ src/lib/home/hero.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+  import { quintIn } from "svelte/easing";
+  import { Icon } from "svelte-hero-icons";
+  import FlyInView from "$lib/transitions-inview/fly.svelte";
+  import FadeInView from "$lib/transitions-inview/fade.svelte";
+  import type { HeroProps } from "./heroItems";
+  export let props: HeroProps;
+  let { icon, heading, text } = props;
+</script>
+
+<section class="tutorial-section">
+  <FlyInView
+    options={{
+      opacity: 0,
+      x: -400,
+      delay: 250,
+      easing: quintIn,
+      duration: 750,
+    }}
+  >
+    <article class="tutorial-icon-container">
+      <Icon src={icon} />
+    </article>
+  </FlyInView>
+
+  <FadeInView
+    options={{
+      delay: 1000,
+      duration: 500,
+    }}
+  >
+    <article class="overflow tutorial-text">
+      <h1 class="heading">
+        {heading}
+        <hr />
+      </h1>
+
+      <p>
+        {text}
+      </p>
+    </article>
+  </FadeInView>
+</section>
+
+<style>
+  p {
+    line-height: 2ch;
+  }
+  .overflow {
+    display: inline-block;
+    overflow: hidden;
+    vertical-align: bottom;
+  }
+
+  .tutorial-section {
+    min-height: 100vh;
+    margin-top: 2rem;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    /* border: 1px solid red; */
+  }
+
+  .tutorial-icon-container {
+    width: 5rem;
+    /* border: 1px solid red; */
+  }
+  .tutorial-text {
+    text-align: center;
+    & h1 {
+      font-size: 2rem;
+    }
+    & p {
+      font-size: 1.25rem;
+    }
+    & hr {
+      border: none;
+      overflow: visible;
+      text-align: center;
+      height: 5px;
+      border-top: 0.1rem dotted white;
+    }
+  }
+  @media only screen and (min-width: 768px) {
+    .tutorial-icon-container {
+      width: 10rem;
+      /* border: 1px solid red; */
+    }
+    .tutorial-text > h1 {
+      font-size: 2.5rem;
+    }
+    .tutorial-text > p {
+      font-size: 1.5rem;
+    }
+  }
+  @media only screen and (min-width: 1024px) {
+    .tutorial-text > h1 {
+      font-size: 2.75rem;
+    }
+    .tutorial-text > p {
+      font-size: 1.75rem;
+    }
+  }
+</style>
---
+++ src/lib/home/heroItems.ts
@@ -0,0 +1,42 @@
+import {
+  CodeBracket,
+  CreditCard,
+  Heart,
+  NoSymbol,
+  ShieldCheck,
+  UserGroup,
+} from "svelte-hero-icons";
+import type { IconSource } from "svelte-hero-icons";
+export interface HeroProps {
+  icon: IconSource;
+  heading: string;
+  text: string;
+}
+
+export const heroItems: HeroProps[] = [
+  {
+    heading: "No Fees",
+    text: "Free for artists and for listeners. No more bills, goodbye spotify",
+    icon: CreditCard,
+  },
+  {
+    heading: "Community-Driven",
+    text: "Users can donate to eachother and the platform to keep it running",
+    icon: Heart,
+  },
+  {
+    heading: "Secured by Stripe",
+    text: "Payment details are secured by Stripe. Parachune does not handle your details",
+    icon: ShieldCheck,
+  },
+  {
+    heading: "No Advertisement",
+    text: "Ad-block can take a break here, we dont run ads. With more users, we will need more donations",
+    icon: NoSymbol,
+  },
+  {
+    heading: "No AI",
+    text: "We are pretty strong on AI and we have no plans to use AI in our codebase",
+    icon: CodeBracket,
+  },
+];
