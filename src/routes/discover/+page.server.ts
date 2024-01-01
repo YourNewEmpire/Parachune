f82bc66d41d7326e149024f2fde4e5423583f7bb
YourNewEmpire <archiesmyth26@googlemail.com>
2024-01-01 16:09:56 +0000

    chore: use generated types in discover page

--- src/routes/discover/+page.server.ts
+++ src/routes/discover/+page.server.ts
@@ -1,32 +1,19 @@
 import { fail } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
-type SongRow = {
-  artist_id: number;
-  created_at: string | null;
-  id: number;
-  name: string | null;
-  song_url: string | null;
-  profiles: {
-    id: string;
-    username: string;
-    avatar_url: string;
-  };
-};
+
 export const prerender = true;
 export const load: PageServerLoad = async ({
   locals: { supabase },
   parent,
 }) => {
-  const { data: songFetch, error: songError } = await supabase
+  const { data: songData, error: songError } = await supabase
     .from("songs")
-    .select(
-      `artist_id, name, song_url, created_at, id, profiles (id, avatar_url, username)`
-    )
+    .select("*")
     .range(0, 4);
 
   const { data: profileData, error: profileError } = await supabase
     .from("profiles")
-    .select(`username, avatar_url, id`)
+    .select(`*`)
     .range(0, 4)
     .not("username", "is", null);
 
@@ -38,7 +25,7 @@ export const load: PageServerLoad = async ({
   }
   //@ts-ignore
   //? So hard to type the data from supabase db fetch. Need to generate types from db.
-  let songData: SongRow[] = [].concat(songFetch);
+
   const { profile, session } = await parent();
   return { songData, profileData, profile, session };
 };
--- src/routes/discover/+page.svelte
+++ src/routes/discover/+page.svelte
@@ -9,34 +9,36 @@
 <svelte:head>
   <title>Discover Music - Parachune</title>
 </svelte:head>
-
-<h1>Discover Music</h1>
-<section class="card-container">
-  {#each songData as song}
-    <SongCard {song} />
-  {/each}
-</section>
-<article style="justify-content: center; display: flex; margin-top: 1rem;">
-  <a class="styled-button" style="width:fit-content" href="/songs"
-    >Find all songs
-  </a>
-</article>
-<h1>Discover Artists</h1>
-
-<section class="card-container">
-  {#each profileData ?? [] as profile}
-    <a href="/profiles/{profile.id}" class="link-card col-container">
-      <AvatarIcon
-        altText={profile.username}
-        size={10}
-        url={profile.avatar_url}
-      />
-      <h1>{profile.username}</h1>
+<section>
+  <h1>Discover Music</h1>
+  <section class="card-container">
+    {#each songData ?? [] as song}
+      <SongCard {song} />
+    {/each}
+  </section>
+  <article style="justify-content: center; display: flex; margin-top: 1rem;">
+    <a class="styled-button" style="width:fit-content" href="/songs"
+      >Find all songs
     </a>
-  {/each}
+  </article>
 </section>
-<article style="justify-content: center; display: flex; margin-top: 1rem;">
-  <a class="styled-button" style="width:fit-content" href="/profiles"
-    >Find all artists
-  </a>
-</article>
+<section>
+  <h1>Discover Artists</h1>
+  <section class="card-container">
+    {#each profileData ?? [] as profile}
+      <a href="/profiles/{profile.id}" class="link-card col-container">
+        <AvatarIcon
+          altText={profile.username ?? "Unkown user"}
+          size={10}
+          url={profile.avatar_url ?? ""}
+        />
+        <h1>{profile.username}</h1>
+      </a>
+    {/each}
+  </section>
+  <article style="justify-content: center; display: flex; margin-top: 1rem;">
+    <a class="styled-button" style="width:fit-content" href="/profiles"
+      >Find all artists
+    </a>
+  </article>
+</section>
