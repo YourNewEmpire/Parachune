f6514725b1535865cad81909c80e4bcf6677bda6
YourNewEmpire <archiesmyth26@googlemail.com>
2024-01-17 18:05:50 +0000

    chore: catch err if profile data fails

--- src/routes/profiles/+page.server.ts
+++ src/routes/profiles/+page.server.ts
@@ -1,3 +1,4 @@
+import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({
@@ -8,6 +9,12 @@ export const load: PageServerLoad = async ({
     .from("profiles")
     .select(`username, avatar_url, id`)
     .not("username", "is", null);
+  if (dbError) {
+    throw error(
+      500,
+      `Error getting profiles data, message: ${dbError.message}`
+    );
+  }
   const { profile, session } = await parent();
   return { profileList, profile, session };
 };
