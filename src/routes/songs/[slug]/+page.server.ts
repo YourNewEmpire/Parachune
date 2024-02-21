515cdd6d990d80f96317ea68927b5f1b305c09c5
YourNewEmpire <archiesmyth26@googlemail.com>
2024-02-21 17:51:36 +0000

    feat: song comments added. unfinished

--- src/routes/songs/[slug]/+page.server.ts
+++ src/routes/songs/[slug]/+page.server.ts
@@ -1,5 +1,5 @@
-import { error, redirect } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types";
+import { error, fail } from "@sveltejs/kit";
+import type { Actions, PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({
   locals: { supabase },
@@ -8,10 +8,12 @@ export const load: PageServerLoad = async ({
 }) => {
   const { data: songData, error: songDataErr } = await supabase
     .from("songs")
-    .select(`*, profiles (id, avatar_url, username)`)
+    .select(
+      `*, profiles (id, avatar_url, username), albums(title, image_url), song_comments(*, profiles(username, avatar_url))`
+    )
     .eq("id", params.slug)
+    .order("created_at", { referencedTable: "song_comments", ascending: false })
     .single();
-
   if (songDataErr) {
     throw error(404, {
       message: "Song not found",
@@ -20,4 +22,120 @@ export const load: PageServerLoad = async ({
 
   const { profile, session } = await parent();
   return { song: songData, profile, session };
+};
+
+export const actions: Actions = {
+  insertComment: async ({ request, locals: { getSession, supabase } }) => {
+    //todo - check that songid is equal
+    const session = await getSession();
+    if (!session) {
+      return fail(400);
+    }
+    const formData = await request.formData();
+    const commentText = formData.get("comment_text") as string;
+    if (commentText.length < 1) {
+      return fail(400);
+    }
+    /* 
+    RUN pnpm add -D @coffeeandfun/google-profanity-words
+    ADD THIS CODE:
+    import { ProfanityEngine } from '@coffeeandfun/google-profanity-words';
+    const profanityEn = new ProfanityEngine({ language: 'en' });
+
+     const hasCurseWords = await profanityEn.hasCurseWords(commentText);
+    */
+    const commentPrivate = formData.get("comment_private");
+    const songId = formData.get("song_id") as string;
+
+    // todo get song id from route params
+    const { data: commentInsert, error: commentErr } = await supabase
+      .from("song_comments")
+      .insert({
+        comment_text: commentText,
+        private: commentPrivate ? true : false,
+        song_id: songId,
+        user_id: session.user.id,
+      })
+      .select("*, profiles(username, avatar_url)");
+
+    if (commentErr) {
+      throw error(500, `error managing comment: ${commentErr.message}`);
+    }
+
+    return {
+      success: true,
+      newComment: commentInsert[0],
+    };
+  },
+  updateComment: async ({ request, locals: { getSession, supabase } }) => {
+    const session = await getSession();
+
+    if (!session) {
+      return fail(400);
+    }
+
+    const formData = await request.formData();
+    // const updateSchema = zfd.formData({
+    //   commentId: zfd.text(),
+    //   commentIndex: zfd.text(),
+    //   commentPrivate: zfd.text(),
+    //   commentText: zfd.text(),
+    // });
+    // const result = updateSchema.safeParse(formData);
+    // if (!result.success) {
+    //   return fail(400);
+    // }
+
+    const commentId = formData.get("update_comment_id") as string;
+    const commentIndex = formData.get("update_comment_index") as string;
+    const commentPrivate = formData.get("update_private") as string;
+    const commentText = formData.get("update_text") as string;
+
+    const { data: commentUpdate, error: commentErr } = await supabase
+      .from("song_comments")
+      .update({
+        comment_text: commentText,
+        private: commentPrivate ? true : false,
+      })
+      .eq("id", commentId)
+      .select("*, profiles(username, avatar_url)");
+
+    if (commentErr) {
+      fail(400);
+    }
+    if (commentUpdate)
+      return {
+        success: true,
+        updatedCommentIndex: commentIndex,
+        updatedComment: commentUpdate[0],
+      };
+    else {
+      fail(400);
+    }
+  },
+  deleteComment: async ({ request, locals: { getSession, supabase } }) => {
+    const session = await getSession();
+
+    if (!session) {
+      return fail(400);
+    }
+
+    const formData = await request.formData();
+    // const deleteSchema = zfd.formData({
+    //   commentId: zfd.text(),
+    // });
+    // const result = deleteSchema.safeParse(formData);
+    // if (!result.success) {
+    //   return fail(400);
+    // }
+    const commentId = formData.get("update_comment_id") as string;
+    const { error } = await supabase
+      .from("song_comments")
+      .delete()
+      .eq("id", commentId);
+    if (error) {
+      return fail(400);
+    }
+    return { success: true, deletedComment: commentId };
+  },
 };
--- src/routes/songs/[slug]/+page.svelte
+++ src/routes/songs/[slug]/+page.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
-  import { Icon, Photo } from "svelte-hero-icons";
   import type { PageData } from "./$types";
   import AvatarIcon from "$lib/avataricon.svelte";
   import Playbutton from "$lib/playbutton.svelte";
   import Queuebutton from "$lib/queuebutton.svelte";
   import Savebutton from "$lib/savebutton.svelte";
-
+  import Albumimage from "$lib/albumimage.svelte";
+  import type { SubmitFunction } from "@sveltejs/kit";
+  import { addToast } from "$lib/stores";
+  import { enhance } from "$app/forms";
+  import tooltip from "$lib/utils/tooltip";
+  import { invalidateAll } from "$app/navigation";
   export let data: PageData;
-  let { song } = data;
+
+  let { song, session } = data;
   let songPlayerData = {
     artistId: song.artist_id ?? "",
     artistName: song.profiles?.username ?? "",
@@ -15,35 +20,172 @@
     name: song.name ?? "",
     songUrl: song.song_url ?? "",
   };
+  let allComments = session?.user
+    ? song.song_comments.filter((c) => c.user_id !== session?.user.id)
+    : null;
+  let userComments = song.song_comments.filter(
+    (c) => c.user_id === session?.user.id
+  );
+
+  // let loading = false;
+
+  let insertTextarea: HTMLTextAreaElement;
+  let insertCheckbox: HTMLInputElement;
+
+  //todo - create handleDelete, handleUpdate and handleInsert. Stop sharing one submit function for many actions...
+
+  const handleUpdate: SubmitFunction = ({ cancel }) => {
+    if (!session) {
+      cancel();
+    }
+    return async ({ result, update }) => {
+      if (result.type === "success") {
+        if (result.data?.updatedComment) {
+          userComments[result.data.updatedCommentIndex] =
+            result.data.updatedComment;
+          addToast({
+            dismissable: true,
+            message: "Comment updated",
+            timeout: 5000,
+            type: "info",
+          });
+        }
+        if (result.data?.deletedComment) {
+          window.location.reload();
+        }
+      } else {
+        addToast({
+          dismissable: true,
+          message:
+            "Failed to update comment, ensure your comment is at least 1 character and does not include profanity. Contact support if there is a clear issue.",
+          timeout: 5000,
+          type: "failure",
+        });
+      }
+      // loading = false;
+    };
+  };
+
+  const handleSubmit: SubmitFunction = ({ cancel }) => {
+    if (!session) {
+      cancel();
+      addToast({
+        dismissable: true,
+        message: "Sign up for an account with Google or Discord in the header",
+        timeout: 5000,
+        type: "failure",
+      });
+    }
+
+    // loading = true;
+    return async ({ result, update }) => {
+      if (result.type === "success") {
+        if (result.data?.newComment) {
+          userComments = [result.data.newComment, ...userComments];
+          insertTextarea.value = "";
+          insertCheckbox.checked = false;
+        } else {
+          addToast({
+            dismissable: true,
+            message:
+              "Comment update succeeded but could not get information from server to update ui",
+            timeout: 5000,
+            type: "failure",
+          });
+        }
+      } else {
+        addToast({
+          dismissable: true,
+          message:
+            "Failed submission, ensure your comment is at least 1 character and does not include profanity. Contact support if there is a clear issue.",
+          timeout: 5000,
+          type: "failure",
+        });
+      }
+      // loading = false;
+    };
+  };
+
+  //make sure deleting works smoothly after edited, random edge case
+  const handleCommentEdit = (id: number) => {
+    let commentCheckBox = document.getElementById(
+      `update_private_${id}`
+    ) as HTMLInputElement;
+    let editBtn = document.getElementById(
+      `update_edit_${id}`
+    ) as HTMLButtonElement;
+    let cancelBtn = document.getElementById(
+      `update_cancel_${id}`
+    ) as HTMLButtonElement;
+    let txtArea = document.getElementById(
+      `update_text_${id}`
+    ) as HTMLTextAreaElement;
+
+    if (
+      txtArea.value !== userComments[id].comment_text ||
+      commentCheckBox.checked !== userComments[id].private
+    ) {
+      editBtn.disabled = false;
+      cancelBtn.disabled = false;
+    } else {
+      editBtn.disabled = true;
+      cancelBtn.disabled = true;
+    }
+  };
+
+  const handleCommentCancel = (e: any) => {
+    let commentIndex = e.target.id.replace("update_cancel_", "");
+    let editBtn = document.getElementById(
+      `update_edit_${commentIndex}`
+    ) as HTMLButtonElement;
+    let cancelBtn = document.getElementById(
+      `update_cancel_${commentIndex}`
+    ) as HTMLButtonElement;
+    let txtArea = document.getElementById(
+      `update_text_${commentIndex}`
+    ) as HTMLTextAreaElement;
+
+    txtArea.value = userComments[commentIndex].comment_text;
+
+    editBtn.disabled = true;
+    cancelBtn.disabled = true;
+  };
 </script>
 
-<div>
-  <h1>
+<section aria-labelledby="song-heading">
+  <h1 id="song-heading">
     {song.name} by
     <a href="/profiles/{song.profiles?.id}" class="styled-link">
       {song.profiles?.username}
     </a>
   </h1>
-  <section class="grid-1">
-    <article
+  <div class="grid-1">
+    <div
       class="card"
       style=" display:flex; flex-direction:column; align-items:center; justify-content: center; "
     >
-      <div
-        class="song-image"
-        style="height: 12em; width: 12em; border-radius: 50%;  background-color: grey;"
-      >
-        <Icon src={Photo} />
-      </div>
-
-      <p>Song/Album photo coming soon with albums</p>
+      <Albumimage
+        url={song.albums?.image_url ?? ""}
+        size={12}
+        altText={`${song.albums?.title} Album Photo` ?? "No title, loading"}
+      />
+      <article>
+        <h1>
+          <span style="font-family: League Spartan, sans-serif;"
+            >Part of {song.profiles?.username}'s Album:
+          </span>
+          <a class="styled-link" href="/albums">
+            {song.albums?.title}
+          </a>
+        </h1>
+      </article>
       <article class="row-container">
         <Playbutton songData={songPlayerData} />
         <Queuebutton songData={songPlayerData} />
         <Savebutton songId={song.id} songUrl={song.song_url ?? ""} />
       </article>
-    </article>
-    <article class="card">
+    </div>
+    <div class="card">
       <AvatarIcon
         altText={song.profiles?.username ?? "No avatar"}
         size={5}
@@ -53,9 +195,129 @@
       <p>
         "{song.lyrics ?? "No lyrics for this one."}"
       </p>
+    </div>
+  </div>
+
+  <section aria-labelledby="comments-heading">
+    <h1 id="comments-heading">Comments</h1>
+
+    <article class="card">
+      <form
+        aria-label="Write a comment"
+        class="input-group"
+        use:enhance={handleSubmit}
+        action="?/insertComment"
+        method="post"
+      >
+        <textarea
+          bind:this={insertTextarea}
+          placeholder="Write your comment..."
+          rows="5"
+          name="comment_text"
+        />
+
+        <div
+          style="width: fit-content;"
+          use:tooltip={{
+            content: "Private to you and the artist",
+          }}
+        >
+          <input
+            bind:this={insertCheckbox}
+            type="checkbox"
+            name="comment_private"
+            id="comment_private_id"
+          />
+          <label for="comment_private_id"> Set Private</label>
+        </div>
+        <input readonly hidden name="song_id" value={song.id} />
+        <button style="width:fit-content" class="styled-button" type="submit">
+          Submit Comment
+        </button>
+      </form>
     </article>
+
+    <section
+      aria-label="List of all comments"
+      style="display:flex; flex-direction: column; gap: 1rem; margin-top: 1rem"
+    >
+      {#each userComments as u, id}
+        <article class="card" id="user_comment_{u.id}">
+          <form
+            method="post"
+            aria-label="Edit Comment: ({u.comment_text})"
+            class="input-group"
+            style="row-gap: 0.5rem;"
+            action="?/updateComment"
+            use:enhance={handleUpdate}
+          >
+            <h1 style="font-size: 1rem;">
+              {u.profiles?.username}
+            </h1>
+            <textarea
+              on:input={() => handleCommentEdit(id)}
+              class="editable-text"
+              id="update_text_{id}"
+              name="update_text"
+              rows="5">{u.comment_text}</textarea
+            >
+            <div style="display: flex; flex-direction: row;">
+              <div>
+                <input
+                  checked={u.private ? true : false}
+                  type="checkbox"
+                  on:input={() => handleCommentEdit(id)}
+                  name="update_private"
+                  id="update_private_{id}"
+                />
+                <label for="update_private_{id}">Set Private</label>
+              </div>
+              <div
+                style="display:flex; flex-direction: row; margin-left:auto; gap: 0.5rem"
+              >
+                <input hidden name="update_comment_index" value={id} />
+                <input hidden name="update_comment_id" value={u.id} />
+
+                <button
+                  id="update_edit_{id}"
+                  type="submit"
+                  class="styled-button"
+                  disabled>Edit</button
+                >
+                <button
+                  id="update_cancel_{id}"
+                  on:click={handleCommentCancel}
+                  class="styled-button"
+                  disabled>Cancel</button
+                >
+                <button
+                  id="update_delete_{id}"
+                  formaction="?/deleteComment"
+                  class="styled-button">Delete</button
+                >
+              </div>
+            </div>
+            <p>
+              {new Date(u.created_at ?? "").toLocaleDateString()}
+            </p>
+          </form>
+        </article>
+      {/each}
+
+      {#each allComments ?? song.song_comments as s}
+        <article class="card">
+          {s.comment_text}
+        </article>
+      {/each}
+      {#if userComments.length === 0 && session}
+        <p>You have no comments on this song yet</p>
+      {/if}
+      {#if song.song_comments.length === 0}
+        <p>There are no comments from others on this song yet</p>
+      {/if}
+    </section>
   </section>
-</div>
+</section>
 
 <style>
   .grid-1 {
@@ -70,5 +332,10 @@
       grid-template-rows: 1fr;
       grid-template-columns: 3fr 1fr;
     }
+  }
+  .editable-text {
+    resize: none;
+    font-family: "League Spartan";
+    /* background: none; */
   }
 </style>
