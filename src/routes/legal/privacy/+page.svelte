c96f3ce6d7b121ff07c31dd38221cef13c68c513
YourNewEmpire <archiesmyth26@googlemail.com>
2024-02-28 18:53:01 +0000

    feat: add new legal pages, wip

---
+++ src/routes/legal/privacy/+page.svelte
@@ -0,0 +1,32 @@
+<article aria-labelledby="privacy-heading">
+  <h1 id="privacy-heading">Parachune Privacy Policy</h1>
+  <p>
+    This is Parachune&trade;. Capitlaised terms used here are defined in the <a
+      class="styled-link"
+      href="/legal/terms">Terms of Use</a
+    >
+  </p>
+  <p>
+    We are hosted on <a class="styled-link" href="https://vercel.com">Vercel</a
+    >, they process information on behalf of their customers. Vercel's
+    <a class="styled-link" href="https://vercel.com/legal/privacy-policy"
+      >privacy policy</a
+    > applies to the users of Parachune. By using the App, you agree to Vercel's
+    privacy policy.
+  </p>
+  <p>TO BE COMPLETED BEFORE LAUNCH</p>
+</article>
+
+<style>
+  a {
+    text-decoration: 0.1rem underline;
+  }
+
+  h1 {
+    font-size: 2rem;
+  }
+
+  p {
+    line-height: 1.5rem;
+  }
+</style>
---
+++ src/routes/legal/terms/+page.svelte
@@ -0,0 +1,23 @@
+<article aria-labelledby="terms-heading">
+  <h1 id="terms-heading">Parachune Terms of Use</h1>
+  <p>
+    This is Parachune&trade;. (company and address TBA) ("Parachune", "we"
+    "our", or "us"). We offer an application (the "App") for artists and music
+    lovers to connect and donate to eachother in a free, streamlined way.
+  </p>
+  <p>
+    These terms of use govern your use of Parachune. This is a legally binding
+    contract (the "Agreement") between the user ("you", "your") and Parachune,
+    for the use of the Parachune app.
+  </p>
+  <p>TO BE COMPLETED BEFORE LAUNCH</p>
+</article>
+
+<style>
+  h1 {
+    font-size: 2rem;
+  }
+  p {
+    line-height: 1.5rem;
+  }
+</style>
