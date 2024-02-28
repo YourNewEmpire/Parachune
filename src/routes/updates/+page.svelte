74809eaad1515b3d09e49bc1029155a3b1c9207f
YourNewEmpire <archiesmyth26@googlemail.com>
2024-02-28 18:54:24 +0000

    feat: add updates page

---
+++ src/routes/updates/+page.svelte
@@ -0,0 +1,45 @@
+<article aria-labelledby="upcoming-heading">
+  <h1 id="upcoming-heading">
+    <span class="circle" style="background-color: blue;"></span>
+    Upcoming features
+  </h1>
+  <ul>
+    <li>
+      Real-time notifications when users comment on your songs while using the
+      app.
+    </li>
+    <li>
+      Something to let the user know it was seen, a like or heart or something.
+    </li>
+    <li>
+      Music player loading state. When you click play on a song, the player
+      slides into view but loading can sometimes take a couple seconds. The
+      interface needs to indicate the loading state to the user.
+    </li>
+    <li>Song queue persisted after leaving the app or refreshing the app</li>
+  </ul>
+</article>
+<article aria-labelledby="notes-heading">
+  <h1 id="notes-heading">
+    <span class="circle" style="background-color: red;"></span> Current issues
+  </h1>
+  <p>None! 🪂 🎛️ 🟢</p>
+</article>
+
+<!-- <article aria-labelledby="notes-heading">
+  <h1 id="notes-heading">Completed features</h1>
+
+</article> -->
+<style>
+  .circle {
+    box-shadow: 0 0 1rem var(--text-color);
+    width: 0.75rem;
+    height: 0.75rem;
+    border-radius: 50%;
+    display: inline-block;
+  }
+  li {
+    margin-top: 0.5rem;
+    line-height: 1.5rem;
+  }
+</style>
